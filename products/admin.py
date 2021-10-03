c854ce283fd3c2f51c8a05cc50be077ae81dd6fc
koomin <bartoszkominko@gmail.com>
2021-10-03 21:41:08 +0200

    Add units.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ProductCategory, Product
+from .models import ProductCategory, Product, Unit
 from warehouse.admin import WarehouseAdmin
 
 
@@ -9,10 +9,16 @@ class ProductCategoryAdmin(WarehouseAdmin):
 
 
 class ProductAdmin(WarehouseAdmin):
-    list_display = ('code', 'name', 'category', 'optima_id',)
+    list_display = ('code', 'name', 'category', 'unit', 'optima_id',)
     search_fields = ['name', 'code', ]
-    list_filter = ['category__name']
+    list_filter = ['category__name', 'unit__short_name']
 
 
+class UnitAdmin(WarehouseAdmin):
+    list_display = ('short_name', 'name')
+    search_fields = ['short_name', 'name']
+
+
+admin.site.register(Unit, UnitAdmin)
 admin.site.register(ProductCategory, ProductCategoryAdmin)
 admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from warehouse.models import WarehouseModel
 
 
+class Unit(WarehouseModel):
+    name = models.CharField(max_length=120)
+    short_name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.name.title()} - {self.short_name}'
+
+
 class ProductCategory(WarehouseModel):
     name = models.CharField(max_length=55, null=False, blank=False)
     product_code = models.CharField(max_length=255, null=False, blank=False)
@@ -15,6 +23,7 @@ class Product(WarehouseModel):
     code = models.CharField(max_length=50, null=False, blank=False)
     optima_id = models.IntegerField(null=False, blank=False)
     category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE, null=True)
+    unit = models.ForeignKey('Unit', on_delete=models.CASCADE, null=True, blank=False)
 
     def __str__(self):
         return f'{self.code} - {self.name}'
