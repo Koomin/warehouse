5028eaa91c013d09b2abf3e53af53dd8e02fe575
koomin <bartoszkominko@gmail.com>
2021-10-03 21:28:38 +0200

    Changes in admin representation.

--- products/admin.py
+++ products/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+from .models import ProductCategory, Product
+from warehouse.admin import WarehouseAdmin
+
+
+class ProductCategoryAdmin(WarehouseAdmin):
+    list_display = ('product_code', 'name',)
+    search_fields = ['name', 'product_code', ]
+
+
+class ProductAdmin(WarehouseAdmin):
+    list_display = ('code', 'name', 'category', 'optima_id',)
+    search_fields = ['name', 'code', ]
+    list_filter = ['category__name']
+
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -6,9 +6,15 @@ class ProductCategory(WarehouseModel):
     name = models.CharField(max_length=55, null=False, blank=False)
     product_code = models.CharField(max_length=255, null=False, blank=False)
 
+    def __str__(self):
+        return f'{self.product_code} - {self.name}'
+
 
 class Product(WarehouseModel):
     name = models.CharField(max_length=255, null=False, blank=False)
     code = models.CharField(max_length=50, null=False, blank=False)
     optima_id = models.IntegerField(null=False, blank=False)
     category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.code} - {self.name}'
