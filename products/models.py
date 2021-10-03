19e7116a86f2ce3b4b42cf1c7c173a69ef4e9fda
koomin <bartoszkominko@gmail.com>
2021-10-03 20:43:47 +0200

    Add django-simple-history. Create ProductCategory model.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=55)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    history = HistoricalRecords()
