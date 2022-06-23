8097ce3e4fc1b6284ce77cae30f779f23a0446fc
Koomin <bartoszkominko@gmail.com>
2022-06-23 19:14:16 +0200

    Create store model.

--- app/warehouse_app/stores/admin.py
+++ app/warehouse_app/stores/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from warehouse_app.stores.models import Store
+from warehouse_app.warehouse.admin import WarehouseAdmin
+
+
+@admin.register(Store)
+class StoreAdmin(WarehouseAdmin):
+    list_display = ('short_name', 'name', 'optima_id', 'status')
+    search_fields = ['short_name']
+    list_filter = ['status']
--- app/warehouse_app/stores/models.py
+++ app/warehouse_app/stores/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from warehouse_app.warehouse.models import WarehouseModel
+
+
+class Store(WarehouseModel):
+    ACTIVE = 0
+    INACTIVE = 1
+    STATUS = (
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive')
+    )
+    optima_id = models.PositiveIntegerField()
+    short_name = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=255, null=True, blank=False)
+    description = models.TextField(null=True, blank=True)
+    register = models.CharField(max_length=50, null=False, blank=False)
+    status = models.IntegerField(choices=STATUS, default=ACTIVE)
