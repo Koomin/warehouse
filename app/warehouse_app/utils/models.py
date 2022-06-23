d2fb2a087d94c4e84ed3e7cb06c001e73b5b928a
Koomin <bartoszkominko@gmail.com>
2022-06-23 19:55:33 +0200

    Prepare store import. Change docker-compose to use static db volume.

--- app/warehouse_app/utils/models.py
+++ app/warehouse_app/utils/models.py
@@ -1,15 +1,12 @@
-from django.db import models
-
-# Create your models here.
+from warehouse_app.stores.models import Store
 from warehouse_app.products.models import Product, Unit
 
 
 class OptimaProduct:
-
     query = 'SELECT Twr_TwrId, Twr_Kod, Twr_Nazwa, Twr_JM, Twr_JMZ, Twr_JMPrzelicznikL, Twr_JMPrzelicznikM' \
             ' FROM CDN.Towary'
 
-    def __init__(self, data_row):
+    def __init__(self, data_row, create=True):
         self.data_row = data_row
         self.optima_id = self.get_optima_id()
         self.optima_code = self.get_optima_code()
@@ -18,7 +15,8 @@ class OptimaProduct:
         self.optima_unit_collective = self.get_optima_unit_collective()
         self.optima_unit_converter = self.get_optima_unit_converter()
         self.optima_unit_converter_collective = self.get_optima_unit_converter_collective()
-        self.create_product()
+        if create:
+            self.create_product()
 
     def __str__(self):
         return f'{self.optima_id} {self.optima_name} - {self.optima_code},' \
@@ -52,9 +50,59 @@ class OptimaProduct:
         return unit
 
     def create_product(self):
-        Product.objects.create(
-            name=self.optima_name,
-            code=self.optima_code,
+        Product.objects.update_or_create(
             optima_id=self.optima_id,
-            unit=self.get_or_create_unit()
+            defaults={
+                'name': self.optima_name,
+                'code': self.optima_code,
+                'unit': self.get_or_create_unit()
+            }
+        )
+
+
+class OptimaStore:
+    query = 'SELECT Mag_MagId, Mag_Symbol, Mag_Nazwa, Mag_Opis, Mag_Rejestr, Mag_NieAktywny FROM CDN.Magazyny'
+
+    def __init__(self, data_row, create=True):
+        self.data_row = data_row
+        self.optima_id = self.get_optima_id()
+        self.optima_short_name = self.get_optima_short_name()
+        self.optima_name = self.get_optima_name()
+        self.optima_description = self.get_optima_description()
+        self.optima_register = self.get_optima_register()
+        self.optima_inactive = self.get_optima_inactive()
+        if create:
+            self.create_store()
+
+    def __str__(self):
+        return self.optima_short_name
+
+    def get_optima_id(self):
+        return self.data_row[0]
+
+    def get_optima_short_name(self):
+        return self.data_row[1]
+
+    def get_optima_name(self):
+        return self.data_row[2]
+
+    def get_optima_description(self):
+        return self.data_row[3]
+
+    def get_optima_register(self):
+        return self.data_row[4]
+
+    def get_optima_inactive(self):
+        return self.data_row[5]
+
+    def create_store(self):
+        Store.objects.update_or_create(
+            optima_id=self.optima_id,
+            defaults={
+                'short_name': self.optima_short_name,
+                'name': self.optima_name,
+                'description': self.optima_description,
+                'register': self.optima_register,
+                'status': self.optima_inactive
+            }
         )
