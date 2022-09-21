16120a622d9b13dc089166d615bad66186c5e548
Koomin <bartoszkominko@gmail.com>
2022-09-21 18:13:27 +0200

    Fix.

--- app/warehouse_app/warehouse_auth/api/authentication.py
+++ app/warehouse_app/warehouse_auth/api/authentication.py
@@ -3,12 +3,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class TokenWithUserObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['user'] = {'tralala': 'starara'}
-        return token
-
     def validate(self, attrs):
         data = super().validate(attrs)
         data['user'] = {'first_name': self.user.first_name, 'last_name': self.user.last_name}
